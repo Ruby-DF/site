39ee6d6a48c5543140fee32d688aff505a960b4d
Matheus Richard <matheusrichardt@gmail.com>
2024-06-26 00:26:26 -0300

    Add GH pages deploy workflow

--- esbuild.config.js
+++ esbuild.config.js
@@ -32,6 +32,7 @@ const build = require("./config/esbuild.defaults.js")
  * @type {BuildOptions}
  */
 const esbuildOptions = {
+  publicPath: "/site/_bridgetown/static",
   plugins: [
     // add new plugins here...
   ],
