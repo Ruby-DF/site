c01cb4af522a1472997828736dabbcb19da52db9
Matheus Richard <matheusrichardt@gmail.com>
2024-07-01 15:31:06 -0300

    No need to import unused css

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,5 +1,5 @@
 import "$styles/index.css"
-import "$styles/syntax-highlighting.css"
+// import "$styles/syntax-highlighting.css"
 
 // Import all JavaScript & CSS files from src/_components
 import components from "$components/**/*.{js,jsx,js.rb,css}"
